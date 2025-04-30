1e4e4bacefe560168df0614a67bfb6c73ca084ac
jy796 <g0909626138@gmail.com>
2025-04-30 12:04:38 +0800

    week11

---
+++ week11-2.cpp
@@ -0,0 +1,17 @@
+/// week11-2.cpp
+/// ㄧΑㄧΑ﹚竡ㄧΑ㊣ (ㄧ计/ㄧΑ)
+#include <stdio.h>
+int addnum(int a, int b) /// 2把计
+{///玡琌 ㄧ计/ㄧΑ ﹚竡
+    printf("瞷秈 addnum() ㄧΑ柑, a:%d b:%d\n", a, b);
+    int x;
+    x = a + b;
+    printf("ㄢ计眔计 %d 盢 return 肚\n", x);
+    return x; ///肚倒耕ノよ
+}
+
+int main()
+{
+    int ans = addnum(10, 30); /// иㄏノ/㊣璹ㄧΑ
+    printf("%d", ans);
+}
